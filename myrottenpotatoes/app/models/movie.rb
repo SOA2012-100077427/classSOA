fda2e525f05e4eb2ee7e96c7be946c828bd0a135
saasbook <saasbook@saasbook.(none)>
2012-12-11 17:26:18 +0800

    Pushed Rottenpotatoes_7

--- myrottenpotatoes/app/models/movie.rb
+++ myrottenpotatoes/app/models/movie.rb
@@ -1,24 +1,26 @@
-class Movie < ActiveRecord::Base # from chp 6.7 stubbing the internet
+class Movie < ActiveRecord::Base # from chp 7.1 
+# place a copy of the following line anywhere inside the Movie class
+#  AND inside the Moviegoer class (idiomatically, it should go right
+#  after 'class Movie' or 'class Moviegoer'):chp 7.3 step 1c.
+  has_many :reviews
 
-  class Movie::InvalidKeyError < StandardError ; end
-
-  def self.api_key
-    'cc4b67c52acb514bdf4931f7cedfd12b' # replace with YOUR Tmdb key 
+  RATINGS = %w[G PG PG-13 R NC-17]  #  %w[] shortcut for array of strings
+  validates :title, :presence => true
+  validates :release_date, :presence => true
+  validate :released_1930_or_later # uses custom validator below
+  validates :rating, :inclusion => {:in => RATINGS}
+  def released_1930_or_later
+    errors.add(:release_date, 'must be 1930 or later') if
+      self.release_date < Date.parse('1 Jan 1930')
   end
-
-  def self.find_in_tmdb(string)
-    Tmdb.api_key = self.api_key
-    begin
-      TmdbMovie.find(:title => string)
-    rescue ArgumentError => tmdb_error
-      raise Movie::InvalidKeyError, tmdb_error.message
-    rescue RuntimeError => tmdb_error
-      if tmdb_error.message =~ /status code '404'/
-        raise Movie::InvalidKeyError, tmdb_error.message
-      else
-        raise RuntimeError, tmdb_error.message
-      end
-    end      
-  end
-  # rest of file elided for brevity
+      
 end
+# try in console:
+m = Movie.new(:title => '', :rating => 'RG', :release_date => '1929-01-01')
+# force validation checks to be performed:
+m.valid?  # => false
+m.errors[:title] # => ["can't be blank"]
+m.errors[:rating] # => ["is not included in the list"]
+m.errors[:release_date] # => ["must be 1930 or later"]
+m.errors.full_messages # => ["Title can't be blank", "Rating is not included in the list", "Release date must be 1930 or later"]
+
